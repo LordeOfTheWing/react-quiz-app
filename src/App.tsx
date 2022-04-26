043cd76e0f32068ec71cd135a7855af23c0e2765
Clinton Mainya <klyntonmainya@gmail.com>
2022-04-26 13:02:00 +0300

    Add app state and render component functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+//OpenTrivia Api Response
+import { fetchQuestions } from './API';
 
-function App() {
+//Components
+import QuestionCard from './components/QuestionCard';
+
+//Types
+import { QuestionState } from './API';
+import { log } from 'console';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, 'easy');
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
-    <>
-      <h2>Hello World</h2>
-    </>
+    <div className='App'>
+      <h1>React Quiz App</h1>
+
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+
+      {!gameOver ? <p className='score'>Score: </p> : null}
+      {loading ? <p>Loading Questions...</p> : null}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNumber={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
+    </div>
   );
-}
+};
 
 export default App;
